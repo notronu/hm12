285437f67da59265805bffb2e54c4e0a9eab8d6e
Chika_Pyu <ya.sasha-mochalov1@yandex.ru>
2023-12-26 20:20:34 +0300

    hm13

--- src/Author.java
+++ src/Author.java
@@ -16,12 +16,11 @@ public class Author {
     }
 
     @Override
-    public boolean equals(Object other) {
-        if (this.getClass() != other.getClass()) {
-            return false;
-        }
-        Author author = (Author) other;
-        return name.equals(author.name);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
     }
 
     @Override
--- src/Book.java
+++ src/Book.java
@@ -31,7 +31,7 @@ public class Book {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Book book = (Book) o;
-        return Objects.equals(name, book.name);
+        return Objects.equals(author, book.author) && Objects.equals(name, book.name);
     }
 
     @Override
